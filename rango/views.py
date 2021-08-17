95760b79a2b382e49ccf9c6efbd93c3690804d89
jino dologmanding <jino.c.dologmanding@gmail.com>
2021-08-17 12:06:03 +0800

    my app first commit

---
+++ rango/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
+
+def about(request):
+    context_dict = {'boldmessage': 'This tutorial has been put together by Jino!'}
+    return render(request,'rango/about.html',context=context_dict)
